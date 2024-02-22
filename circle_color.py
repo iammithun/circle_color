e8e560cd8ddecdbddce668c4e2753fbf836e61cb
iammithun <157653522+iammithun@users.noreply.github.com>
2024-02-22 18:20:09 -0500

    Add files via upload

---
+++ circle_color.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 18:06:59 2024
+
+@author: iamrs
+"""
+
+import turtle
+import colorsys
+
+t=turtle.Turtle()
+s=turtle.Screen().bgcolor('black')
+t.speed(0)
+n=70
+h=0
+for i in range (360):
+    c=colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+=1/n
+    t.color(c)
+    t.left(1)
+    for j in range (2):
+        t.left(2)
+        t.circle(100)
+    
